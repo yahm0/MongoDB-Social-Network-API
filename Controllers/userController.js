4f93df37d07b75ad9b2af8435df76afb8d1c604b
yahm0 <stnt190@outlook.com>
2024-05-19 10:27:52 -0400

    added logic to userController.js to update a user by id.

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -32,3 +32,15 @@ const userController = {
             .then(user => res.json(user))
             .catch(err => res.status(400).json(err));
     },
+
+    // Update a user by id
+    updateUser(req, res) {
+        User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user found with this id!' });
+                }
+                res.json(user);
+            })
+            .catch(err => res.status(500).json(err));
+    },
