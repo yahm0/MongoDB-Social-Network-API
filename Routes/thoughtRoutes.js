32ca2856239daeb13b4771c2c7b1b961f5b6b138
yahm0 <stnt190@outlook.com>
2024-05-19 11:24:29 -0400

    made updates tot he thoughtRoutes.js file to import correctly.

--- Routes/thoughtRoutes.js
+++ Routes/thoughtRoutes.js
@@ -1,5 +1,15 @@
+// Routes/thoughtRoutes.js
+
 const router = require('express').Router();
-const { getAllThoughts, getThoughtById, createThought, updateThought, deleteThought } = require('../Controllers/thoughtController');
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../Controllers/thoughtController');
 
 router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
