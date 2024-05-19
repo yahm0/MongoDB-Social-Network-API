56d3329df957e1ba372b19646f75d3af68588ad3
yahm0 <stnt190@outlook.com>
2024-05-19 10:28:40 -0400

    added logic to userController.js to add a friend.

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -60,3 +60,15 @@ const userController = {
             })
             .catch(err => res.status(500).json(err));
     },
+
+    // Add a friend
+    addFriend(req, res) {
+        User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId } }, { new: true })
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user found with this id!' });
+                }
+                res.json(user);
+            })
+            .catch(err => res.status(500).json(err));
+    },
