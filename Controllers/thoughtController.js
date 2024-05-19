6f229e814dd8806662180e97e8920203065f301e
yahm0 <stnt190@outlook.com>
2024-05-19 10:25:12 -0400

    added logic to thoughtController.js to add a reaction.

--- Controllers/thoughtController.js
+++ Controllers/thoughtController.js
@@ -61,3 +61,15 @@ const thoughtController = {
             })
             .catch(err => res.status(500).json(err));
     },
+
+    // Add a reaction
+    addReaction(req, res) {
+        Thought.findByIdAndUpdate(req.params.thoughtId, { $push: { reactions: req.body } }, { new: true })
+            .then(thought => {
+                if (!thought) {
+                    return res.status(404).json({ message: 'No thought found with this id!' });
+                }
+                res.json(thought);
+            })
+            .catch(err => res.status(500).json(err));
+    },
