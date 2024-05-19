d1959f843e4b93b7b954848b8b36abf8869132ff
yahm0 <stnt190@outlook.com>
2024-05-19 10:26:59 -0400

    added logic to userController.js to get a single user by id.

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -10,3 +10,17 @@ const userController = {
             .then(users => res.json(users))
             .catch(err => res.status(500).json(err));
     },
+
+     // Get a single user by id
+     getUserById(req, res) {
+        User.findById(req.params.id)
+            .populate('thoughts')
+            .populate('friends')
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user found with this id!' });
+                }
+                res.json(user);
+            })
+            .catch(err => res.status(500).json(err));
+    },
